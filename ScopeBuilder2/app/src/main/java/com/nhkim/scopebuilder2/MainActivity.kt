2bf44d59bc84212cb34e6efa5060a0ebc1dee7cc
nhkim <nhkim1529@gmail.com>
2023-10-12 14:15:52 +0900

    ScopeBuilder2 example commit

---
+++ ScopeBuilder2/app/src/main/java/com/nhkim/scopebuilder2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nhkim.scopebuilder2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nhkim.scopebuilder2.ui.theme.ScopeBuilder2Theme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+suspend fun doOneTwoThree() = coroutineScope {
+    val job = launch {
+        println("launch1: ${Thread.currentThread().name}")
+        delay(1000L)
+        println("3!")
+    }
+    job.join()
+
+    launch {
+        println("launch2: ${Thread.currentThread().name}")
+        println("1!")
+    }
+
+    launch {
+        println("launch3: ${Thread.currentThread().name}")
+        delay(500L)
+        println("2!")
+    }
+
+    println("4!")
+}
+
+fun main() = runBlocking {
+    doOneTwoThree()
+    println("runBlocking: ${Thread.currentThread().name}")
+    println("5!")
+
+}

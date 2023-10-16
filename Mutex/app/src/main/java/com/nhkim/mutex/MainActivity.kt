8050f9eb5c98acb35e08f44ef7e1977d0a13ab5e
nhkim <nhkim1529@gmail.com>
2023-10-17 00:43:06 +0900

    Mutex example commit

--- Mutex/app/src/main/java/com/nhkim/mutex/MainActivity.kt
+++ Mutex/app/src/main/java/com/nhkim/mutex/MainActivity.kt
@@ -11,11 +11,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nhkim.mutex.ui.theme.MutexTheme
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.system.measureTimeMillis
 
 suspend fun massiveRun(action: suspend () -> Unit){
@@ -33,13 +39,30 @@ suspend fun massiveRun(action: suspend () -> Unit){
     println("$elapsed ms 동안 ${n*k}개의 액션을 수행했습니다.")
 }
 
-var counter = 0
+sealed class CounterMsg
+object IncCounter: CounterMsg()
+class GetCounter(val response: CompletableDeferred<Int>): CounterMsg()
 
-fun main() = runBlocking {
-    withContext(Dispatchers.Default){
-        massiveRun {
-            counter++
+fun CoroutineScope.counterActor() = actor<CounterMsg>{
+    var counter = 0
+    for (msg in channel){
+        when(msg){
+            is IncCounter -> counter++
+            is GetCounter -> msg.response.complete(counter)
         }
     }
-    println("counter = $counter")
+}
+
+fun main() = runBlocking<Unit> {
+    val counter = counterActor()
+    withContext(Dispatchers.Default){
+        massiveRun {
+            counter.send(IncCounter)
+        }
+    }
+
+    val response = CompletableDeferred<Int>()
+    counter.send(GetCounter(response))
+    println("Counter = ${response.await()}")
+    counter.close()
 }
